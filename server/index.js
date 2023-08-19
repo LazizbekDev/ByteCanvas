f60e4ce626a30f85f943144599108b7935e93caf
Lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-08-19 08:34:06 +0500

    api public page

--- server/index.js
+++ server/index.js
@@ -2,15 +2,20 @@ import express from "express";
 import { config } from "dotenv";
 import colors from "colors";
 import postRoutes from "./routes/posts.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 config()
 const app = express();
-
 app.use(express.json());
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.send("Hey")
-})
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 app.use('/api/posts', postRoutes)
 
